ed1f64c9e5a31e9b27dae7a3583c028e3eaca070
SRINIDHII VENKATESH CHANNAGIRI <srinidhii1995@gmail.com>
2019-02-21 19:10:41 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        SudokuSolution sudokuSolution = new SudokuSolution();
+
+        // Storing size of matrix
+        sudokuSolution.sizeOfMatrix = Character.getNumericValue(args[0].toCharArray()[0]);
+
+        // Storing the allowed Symbols list
+        for (char a : args[1].toCharArray()) {
+            sudokuSolution.allowedSymbols.add(a);
+        }
+
+        // Preparing the Sudoku Matrix
+        int index = 2;
+        for (int i = 0; i < sudokuSolution.sizeOfMatrix; i++) {
+            for (int j = 0; j < sudokuSolution.sizeOfMatrix; j++) {
+                if (args[index].toCharArray()[j] == '.')
+                    sudokuSolution.sudokuMatrix[i][j] = '0';
+                else
+                    sudokuSolution.sudokuMatrix[i][j] = args[index].toCharArray()[j];
+            }
+            index++;
+        }
+
+        for (int i = 0; i < sudokuSolution.sizeOfMatrix; i++) {
+            for (int j = 0; j < sudokuSolution.sizeOfMatrix; j++) {
+                System.out.print(sudokuSolution.sudokuMatrix[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("Size of the sudoku matrix = " + args[0]);
+        System.out.println("List of allowed symbols = " + args[1]);
+
+    }
+}
---
+++ src/SudokuSolution.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SudokuSolution implements SudokuSolutionInterface {
+    List<Character> allowedSymbols = new ArrayList<>();
+    Integer sizeOfMatrix = 9;
+
+    Character[][] sudokuMatrix = new Character[sizeOfMatrix][sizeOfMatrix];
+
+    @Override
+    public void computeSolution() {
+
+    }
+}
